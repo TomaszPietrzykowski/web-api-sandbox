f3d103d6195f2ef2c80ba958782ba9218f4f534d
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2023-08-10 21:51:53 +0200

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiSandbox.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastModifiedAt { get; set; }
+    }
+}

43dcbc173ef28e2cced4978c52a8dd3cb8a8bbc7
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2023-08-10 23:49:00 +0200

    table join

--- Models/Product.cs
+++ Models/Product.cs
@@ -5,6 +5,8 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime CreatedAt { get; set; }
-        public int PriceInCents { get; set; } 
+        public ICollection<Review> Reviews { get; set; } 
+        public ICollection<ProductCategory> ProductCategories { get; set; }
+        public ICollection<ProductProducer> ProductProducer { get; set; }
     }
 }
---
+++ Models/ProductProducer.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApiSandbox.Models
+{
+    public class ProductProducer
+    {
+        public int ProductId { get; set; }
+        public int ProducerId { get; set; }
+        public Product Product { get; set; }
+        public Producer Producer { get; set; }
+    }
+}
+// not really many-to-many, doesn't make sense
+// or does it
